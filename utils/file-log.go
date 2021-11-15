8a1392355e7d5f245934ec598c48180ee4ecc258
Haart <mopno1@163.com>
2021-11-15 20:13:02 +0800

    Update log

--- utils/file-log.go
+++ utils/file-log.go
@@ -4,37 +4,59 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 )
 
 type RollingFileLoggerWriter struct {
-	date    time.Time // 日志的当前日期。
-	Pattern string    // 日志使用的文件名模式。
-	file    *os.File  // 当前的写入文件名。
+	date    time.Time  // 日志的当前日期。
+	Pattern string     // 日志使用的文件名模式。
+	file    *os.File   // 当前的写入文件名。
+	lock    sync.Mutex // 同步锁。
 }
 
 func (fl *RollingFileLoggerWriter) Write(p []byte) (n int, err error) {
 	today := time.Now().Truncate(24 * time.Hour)
 	if today != fl.date {
-		if fl.file != nil {
-			fl.file.Sync()
-			fl.file.Close()
-		}
+		fl.closeFile()
 		fl.date = today
 	}
+
+	if err := fl.ensureFileIsOpened(); err != nil {
+		return 0, err
+	}
+
+	return fl.file.Write(p)
+}
+
+func (fl *RollingFileLoggerWriter) closeFile() {
+	fl.lock.Lock()
+	defer fl.lock.Unlock()
+
+	if fl.file != nil {
+		fl.file.Sync()
+		fl.file.Close()
+		fl.file = nil
+	}
+}
+
+func (fl *RollingFileLoggerWriter) ensureFileIsOpened() error {
+	fl.lock.Lock()
+	defer fl.lock.Unlock()
+
 	if fl.file == nil {
 		fileName := fl.createFileName()
 		dir := filepath.Dir(fileName)
 		os.MkdirAll(dir, 0644)
 
 		if f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err != nil {
-			return 0, err
+			return err
 		} else {
 			fl.file = f
 		}
 	}
 
-	return fl.file.Write(p)
+	return nil
 }
 
 func (fl *RollingFileLoggerWriter) createFileName() string {
